e9451163242d9e43725362302c1a2945a76e44a2
Nicolas Sarchi <nicolassarchipicp@gmail>
2023-06-01 15:49:51 -0500

    DOM basico

---
+++ DOM/calculadora/calculadora.js
@@ -0,0 +1,3 @@
+// 2. **Manipulación del DOM**: Utiliza JavaScript para seleccionar los elementos necesarios del DOM, es decir, los elementos HTML que representan los números, operadores y función especial. Puedes utilizar los métodos `getElementById`, `getElementsByClassName` o `querySelector` para seleccionar los elementos.
+
+const u = 0
