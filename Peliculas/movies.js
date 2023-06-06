492f241ed528d23e85c50a49ec07456a0bc34da5
Nicolás <nicolassarchipico@gmail.com>
2023-06-06 13:55:38 -0500

    Movies

--- Peliculas/movies.js
+++ Peliculas/movies.js
@@ -1,53 +1,37 @@
-const peliculas = [];
+const peliculas = new Map ();
 
-function agregarPelicula (){
-    const pelicula = new Map ();
+function agregarPelicula (event){
+    event.preventDefault();
 
     const titulo = document.getElementById('titulo').value,
     genero = document.getElementById('genero').value,
     duracion = document.getElementById('duracion').value,
     autor = document.getElementById('autor').value;
 
-    pelicula.set("Titulo", titulo);
-    pelicula.set("Gènero", genero);
-    pelicula.set("Duracion", duracion);
-    pelicula.set("Autor", autor);
+    
+    peliculas.set(titulo, {genero, duracion, autor});
+    
 
     document.querySelector("#titulo").value = "";
     document.querySelector("#genero").value = "";
     document.querySelector("#duracion").value = "";
     document.querySelector("#autor").value = "";
 
-    peliculas.push(pelicula)
-    
-    const mostrarPeliculas = document.querySelector('.mostrar-peliculas');
-
-    const nuevaPelicula = document.createElement('div');
-    nuevaPelicula.classList.add('card', 'text-center', 'col-sm-6º');
-
-    nuevaPelicula.innerHTML = `<div class="card-header">
-    ${genero}
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">${titulo}</h5>
-    <p class="card-text">${autor}</p>
-    <a href="#" class="btn btn-danger">eliminar</a>
-  </div>
-  <div class="card-footer text-muted">
-    ${duracion} min
-  </div>`
-
-  mostrarPeliculas.appendChild(nuevaPelicula);
-
-  const btnEliminar = nuevaTarea.querySelector('#btn-eliminar');
-  btnEliminar.addEventListener('click', eliminarTarea);
 }
 
-const formulario = document.getElementById('formulario');
+function eliminarPelicula (e){
+  e.preventDefault();
 
-formulario.addEventListener('submit', function(e){
-    e.preventDefault();
-    agregarPelicula();
-    
-    
-})
+  const titulo = document.getElementById('tituloEliminar').value,
+  mensaje = document.createElement('span'),
+  divEliminar = document.getElementById('eliminarPelicula');
+
+  if (peliculas.has(titulo)){
+    peliculas.delete(titulo);
+    mensaje.innerText = `la película ${titulo} ha sido eliminada correctamente`
+  }
+  else{
+    mensaje.innerText = `no Existe una pelicula con el titulo ${titulo}`
+  }
+  divEliminar.appendChild(mensaje);
+}
