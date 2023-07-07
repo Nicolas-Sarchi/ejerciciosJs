ef63fd42e8f2e93774a5f88dd19a3b419066932a
Nicolas Sarchi <nicolassarchipico@gmail.com>
2023-07-07 17:29:03 -0500

    ds

--- API/app.js
+++ API/app.js
@@ -1,6 +1,13 @@
-const getMovies = async () => {
+let botonNext, botonPrev;
+const botones= document.getElementById('botones')
+const pelicula = document.getElementById('pelicula'),
+buscar =document.getElementById('buscar')
 
-    const url = 'https://moviesdatabase.p.rapidapi.com/titles';
+let titulo = "cars";
+const url = `https://moviesdatabase.p.rapidapi.com/titles/search/title/${titulo}?exact=false&titleType=movie`;
+
+const getMovies = async (url) => {
+
     const options = {
         method: 'GET',
         headers: {
@@ -12,12 +19,46 @@ const getMovies = async () => {
     try {
         const response = await fetch(url, options);
         const result = await response.json();
-        console.log(result.results);
+        console.log(result);
+        var contenedor = document.getElementById('contenedor');
+        
+        for(let pelicula of result.results){
+
+        if(pelicula.primaryImage){
+            let divPelicula = `
+            <div class="card border border-0 shadow-lg p-3 mb-5 bg-body-tertiary rounded col-6 mx-auto mt-3" style="width: 18rem;" id="${result.name}">
+            <img src= ${pelicula.primaryImage.url} class="card-img-top" alt="...">
+            <div class="card-body text-center">
+              <h5 class="card-title">${pelicula.titleText.text}</h5>
+              
+            </div>
+          `;
+            
+            contenedor.innerHTML += divPelicula
+        }else{
+            console.log("no");
+        }
+        
+        }
+
+        botonNext = result.next ? `<button type="button" data-url = https://moviesdatabase.p.rapidapi.com${result.next} class="btn btn-primary col-12">Cargar Màs</button>`:"";
+        botones.innerHTML =  botonNext; 
     } catch (error) {
         console.error(error);
     }
 
 }
 
-getMovies()
+buscar.addEventListener("click", ()=>{
+    getMovies(url)
+})
 
+
+botones.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('btn')){
+        let value = e.target.dataset.url
+        console.log(value);
+        getMovies(value)
+    }
+
+})
--- API/clima/script.js
+++ API/clima/script.js
@@ -1,10 +1,11 @@
-async function traerDatos (){
+async function traerDatos(ciudad){
     try{
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=peru&units=metric&appid=2fecb748b5b060c588450f540e21822c`
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&appid=2fecb748b5b060c588450f540e21822c`
         );
         const data = await response.json()
         if (data.cod === '404') {
-            container.innerHTML="No se encuentra la ciudad :(";
+            var container = document.getElementById('contenedor')
+            container.innerHTML = "<div class='alert alert-danger text-center' role='alert'>No se pudo encontrar la ciudad. Por favor, intenta con otra ciudad.</div>";
             return;
         }
         console.log(data);
@@ -15,28 +16,25 @@ async function traerDatos (){
     }
 }
 
-traerDatos()
 async function mostrarDatos (datos){
     try{
         var container = document.getElementById('contenedor')
         container.innerHTML = `
         
-  <div class="card border border-0 shadow-lg p-3 mb-5 text-white bg-dark rounded color-white col-6 mx-auto mt-3" style="width: 18rem;" >
+  <div class="card border border-0 shadow-lg p-3 mb-5 text-white bg-primary rounded color-white col-6 mx-auto mt-3" style="width: 18rem;" >
   <img src= https://openweathermap.org/img/wn/${datos.weather[0].icon}@2x.png class="card-img-top" alt="...">
 
   <div class="card-body text-center">
 
-  <h5 class="card-title">${datos.name}</h5>
-  <p>${datos.main.temp.toFixed(0)}ºC</p>
-  <p>${datos.sys.country}</p>
-  <p>${datos.weather[0].main}</p>
-  <p>
-  <i class="fa-solid fa-wind"></i>
+  <h5 class="card-title fs-3">${datos.name}</h5>
+  <p class = "fs-5 mt-3 ">${datos.main.temp.toFixed(0)}ºC</p>
+  <p class = "fs-5 ">${datos.sys.country}</p>
+  <p class = "fs-5 ">${datos.weather[0].main}</p>
+  <p class = "fs-5 ">
+  <i class="fa-solid fa-wind "></i>
   ${datos.wind.speed} m/s
   </p>
-
-  <p>
-  💧${datos.main.humidity} %
+  <p class = "fs-5 ">  💧${datos.main.humidity} %
   </p>
   
   
@@ -53,5 +51,8 @@ async function mostrarDatos (datos){
 
 
 
-
+document.getElementById('buscar').addEventListener('click', function() {
+    const ciudad = document.getElementById('ciudad').value;
+    traerDatos(ciudad);
+});
 
